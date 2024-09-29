c6c1f365c5c51a2afabbdd88616aba18e15e0ee3
jspinerola <144980273+jspinerola@users.noreply.github.com>
2024-09-29 17:21:32 -0500

    Added comments to Insertion Sort

--- src/Machine.java
+++ src/Machine.java
@@ -13,16 +13,24 @@ public class Machine {
         buffer.add(packet);
     }
     public void dequeue(){
-        Packet packet = buffer.removeFirst();
-        memory.add(0, packet);
-        for (int i = 1; i < memory.size(); i++) {
-            Packet current = memory.get(i);
-            int step = i;
+        Packet packet = buffer.removeFirst(); //get packet from memory and assign it to variable "packet"
+        memory.add(0, packet); //Add packet to the first index in the memory
+        for (int i = 1; i < memory.size(); i++) { //For all packets in the memory, **starting at 1**
+            Packet current = memory.get(i); //Get packet at current index
+            int step = i; //Set step variable to current loop index
+
+            /*
+            While loop. While the step is greater than 0 AND the packet to the left of the current packet's position is
+            greater than the position of the current packet
+            */
             while(step > 0 && memory.get(step - 1).getPosition() > current.getPosition()){
-                memory.set(step, memory.get(step - 1));
-                step--;
+                memory.set(step, memory.get(step - 1)); //Set the value of the packet at location step to the value of
+                                                        //the packet with the value greater than the packet at step.
+
+                step--; //Decrement step. This ensures all values are properly shifted.
             }
-           memory.set(step, current);
+           memory.set(step, current);   //Once shifting is done and space is open, sets current packet to the location
+                                        //of step.
         }
     }
 
